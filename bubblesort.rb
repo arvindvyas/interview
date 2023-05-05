311a40c04303513f26722d20f56e332a9535c87a
Arvind Vyas <=>
2023-05-05 22:48:34 +0530

    added pogram for bubble sort

---
+++ bubblesort.rb
@@ -0,0 +1,17 @@
+
+
+def bubble_sort(list)
+  return list if list.size <=1 # only one record available 
+  swapped = true 
+  
+  while swapped do 
+    swapped = false
+    0.upto(list.size-2) do |i|
+      if list[i] > list[i+1]
+        list[i], list[i+1] = list[i+1], list[i] # swap values
+        swapped = true
+      end
+    end
+  end
+  list
+end
